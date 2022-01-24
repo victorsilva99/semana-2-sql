254e4f8b16f88ee364c4bf6b49b1e1575317bb03
Victor Silva de Souza <fe605@t-systems.com.br>
2022-01-24 10:32:20 -0300

    [FIX] Correção comandos da query

--- exercicio-semana-2.sql
+++ exercicio-semana-2.sql
@@ -1,28 +1,28 @@
 /* Criação do Banco de dados*/
 CREATE DATABASE semana2;
-/* Criação das Tabelas*/
+/* Criação da Tabela Modelo*/
 CREATE TABLE Modelo
 (
 	Id_Modelo INT IDENTITY (1,1) PRIMARY KEY,
 	Descricao VARCHAR(255),
 	Nome VARCHAR(255)
 );
-
+/* Criação da Tabela Eixo*/
 CREATE TABLE Eixo 
 (
 	Id_Eixo INT IDENTITY(1,1) PRIMARY KEY,
 	Fab_Eixo CHAR(11),
 	Id_Caminhao INT
 );
-
+/* Criação da Tabela Caminhao*/
 CREATE TABLE Caminhao
 (
 	Num_Prod INT IDENTITY (1,1) PRIMARY KEY,
 	Data_Fab CHAR(10),
 	Id_Modelo INT FOREIGN KEY REFERENCES Modelo(Id_Modelo),
-	Id_Eixo INT FOREIGN KEY REFERENCES Eixo(Num_Prod) UNIQUE,
+	Id_Eixo INT FOREIGN KEY REFERENCES Eixo(Id_Eixo) UNIQUE,
 );
-/* Criação da Chave Estrangeira da Taela Eixo*/
+/* Criação da Chave Estrangeira (Id_Caminhao) da Tabela Eixo com a chave primária da tabela Caminhao (Num_Pro)*/
 ALTER TABLE Eixo
 ADD CONSTRAINT FK_Eixo_Caminhao FOREIGN KEY (Id_Caminhao) REFERENCES Caminhao (Num_Prod)
 
@@ -40,7 +40,7 @@ INSERT INTO Modelo (Descricao, Nome) VALUES
 ('Caminhão Pesado', 'Volks Constellation 19.360');
 
 /* Inserção de dados da tabela Eixo*/
-INSERT INTO Eixo (Data_Fab) VALUES
+INSERT INTO Eixo (Fab_Eixo) VALUES
 ('01/01/2022'),
 ('02/01/2022'),
 ('03/01/2022'),
@@ -55,6 +55,8 @@ INSERT INTO Eixo (Data_Fab) VALUES
 /* Inserção de dados da tabela Caminhao*/
 INSERT INTO Caminhao (Data_Fab, Id_Modelo, Id_Eixo) VALUES
 (GETDATE(),1,2);
+
+SELECT * FROM Eixo
 
 /* Exercicio 1*/
 SELECT Num_Prod, Data_Fab, nome FROM caminhao
